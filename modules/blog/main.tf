cfe53110f871494d4e29706954f17393935fef9e
Jamie Kay <JamieKayBCook@gmail.com>
2026-02-25 17:00:32 -0600

    fix

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -14,6 +14,7 @@ data "aws_ami" "app_ami" {
   owners = [var.ami_filter.owner]
 }
 
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -32,55 +33,6 @@ module "blog_vpc" {
   }
 }
 
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-  name = "${var.environment.name}-blog"
-
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-  
-  name    = "${var.environment.name}-blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-
-  security_groups = [module.blog_sg.security_group_id]
-
-  listeners = {
-    blog-http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_arn = aws_lb_target_group.blog.arn
-      }
-    }
-  }
-
-
-
-  tags = {
-    Environment = var.environment.name
-  }
-}
-
-resource "aws_lb_target_group" "blog" {
-  name     = "${var.environment.name}-blog"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
@@ -89,18 +41,61 @@ module "blog_autoscaling" {
 
   min_size =var.min_size
   max_size = var.max_size
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-
-  launch_template_name = "blog"
+  vpc_zone_identifier  = module.blog_vpc.public_subnets
+  target_group_arns    = module.blog_alb.target_group_arns
   security_groups      = [module.blog_sg.security_group_id]
   instance_type        = var.instance_type
-
-  image_id = data.aws_ami.app_ami.id
+  image_id             = data.aws_ami.app_ami.id
 
   traffic_source_attachments = {
     "${var.environment.name}-blogblog-alb" = {
      traffic_source_identifier = aws_lb_target_group.blog.arn
     }
   }
+}
+
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+  
+  name    = "${var.environment.name}-blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = var.environment.name
+  }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id              = module.blog_vpc.vpc_id
+  name                = "${var.environment.name}-blog"
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
